a16b52bca1b342d275d0453faec20ad01dae5d9e
Astrid Sofie Thuesen <astridthuesen@hotmail.com>
2024-03-19 09:52:07 +0100

    Teeeest

---
+++ Teeeset_program/Teeeset_program/Teeeset_program/Form1.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.DataFormats;
+
+namespace Teeeset_program
+{ 
+    public partial class Form1 : Form
+    {
+
+        private teeeest _teeeest = new teeeest();
+
+        private connectionHandler _connectionHandler = new connectionHandler();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CONNECTIONSTATUS status = _connectionHandler.ConnectAndGainMastership();
+            lbl_ConnectDisplay.Text = status.ToString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _teeeest.moving();
+        }
+    }
+}
---
+++ Teeeset_program/Teeeset_program/Teeeset_program/teeeest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PMCLIB;
+
+namespace Teeeset_program
+{
+    internal class teeeest
+    {
+        private XBotCommands _xbotCommand = new XBotCommands();
+
+        public int[] GetXbotIds()
+        {
+            XBotIDs tempId = _xbotCommand.GetXBotIDS();
+
+            int[] xbotIds = tempId.XBotIDsArray;
+
+            return xbotIds;
+
+        }
+        public void moving()
+        {
+            int[] xbotIdArray = GetXbotIds();
+
+            _xbotCommand.LinearMotionSI(0, xbotIdArray[0], POSITIONMODE.ABSOLUTE, LINEARPATHTYPE.DIRECT, 0.2, 0.2, 0, 0.05, 0.1);
+            _xbotCommand.LinearMotionSI(0, xbotIdArray[1], POSITIONMODE.ABSOLUTE, LINEARPATHTYPE.DIRECT, 0.24, 0.2, 0, 0.05, 0.1);
+
+            _xbotCommand.LinearMotionSI(0, xbotIdArray[0], POSITIONMODE.RELATIVE, LINEARPATHTYPE.DIRECT, -0.04, 0, 0, 0.05, 0.1);
+        }
+        // 6 cm
+    }
+}
