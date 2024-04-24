1351e6e21c6d66ed6f18a849a0d9ea5b954ae9ff
Skepingawich <93713249+Skepingawich@users.noreply.github.com>
2024-04-24 18:21:21 +0200

    Added Demo of tub handling and removal of nest
    Added a 9th case in the demo which executes an entire demo of a  tub being handled from the magazine to nest removal.
    - Changed multiple classes return from void to double in order to access their MtnRtn time in the main program

--- simple_pmc_mover/simple_pmc_mover/Movement.cs
+++ simple_pmc_mover/simple_pmc_mover/Movement.cs
@@ -37,8 +37,10 @@ namespace simple_pmc_mover
         /// <summary>
         /// Moves all of the given xbots in the same direction based on the distance given
         /// </summary>
-        public void MoveRelativeTogether(int[] xbots, double distance, DIRECTION direction, double maxSpeed, double maxAcc)
+        public double MoveRelativeTogether(int xbot1, int xbot2, double distance, DIRECTION direction, double maxSpeed, double maxAcc)
         {
+            int[] xbots = {xbot1, xbot2 };
+            double total_motion_time = 0;
             switch (direction)
             {
                 case DIRECTION.X:
@@ -46,6 +48,7 @@ namespace simple_pmc_mover
                     for (int i = 0; i < xbots.Length; i++)
                     {
                         MotionRtn xbot = _xbotCommand.LinearMotionSI(0, xbots[i], POSITIONMODE.RELATIVE, LINEARPATHTYPE.YTHENX, distance, 0, 0, maxSpeed, maxAcc);
+                        total_motion_time = xbot.TravelTimeSecs;
                     }
                     break;
                 case DIRECTION.Y:
@@ -53,14 +56,18 @@ namespace simple_pmc_mover
                     for (int i = 0; i < xbots.Length; i++)
                     {
                         MotionRtn xbot = _xbotCommand.LinearMotionSI(0, xbots[i], POSITIONMODE.RELATIVE, LINEARPATHTYPE.YTHENX, 0, distance, 0, maxSpeed, maxAcc);
+                        total_motion_time = xbot.TravelTimeSecs;
                     }
                     break;
             }
+
+            return total_motion_time;
         }
 
 
-        public void MoveOpposite(int first_xbot, int second_xbot, double distance, DIRECTION direction, double maxSpeed, double maxAcc)
+        public double MoveOpposite(int first_xbot, int second_xbot, double distance, DIRECTION direction, double maxSpeed, double maxAcc)
         {
+            double total_motion_time = 0;
 
             switch (direction)
             { 
@@ -68,14 +75,17 @@ namespace simple_pmc_mover
 
                     MotionRtn first_xbotX = _xbotCommand.LinearMotionSI(0, first_xbot, POSITIONMODE.RELATIVE,0,-distance,0,0,maxSpeed,maxAcc);
                     MotionRtn second_xbotX = _xbotCommand.LinearMotionSI(0, second_xbot, POSITIONMODE.RELATIVE, 0, distance, 0, 0, maxSpeed, maxAcc);
+                    total_motion_time = second_xbotX.TravelTimeSecs;
                     break;
 
                 case DIRECTION.Y:
                     MotionRtn first_xbotY = _xbotCommand.LinearMotionSI(0, first_xbot, POSITIONMODE.RELATIVE, 0, 0, -distance, 0, maxSpeed, maxAcc);
                     MotionRtn second_xbotY = _xbotCommand.LinearMotionSI(0, second_xbot, POSITIONMODE.RELATIVE, 0, 0, distance, 0, maxSpeed, maxAcc);
+                    total_motion_time = second_xbotY.TravelTimeSecs;
                     break;
 
             }
+            return total_motion_time;
         }
 
 
@@ -90,10 +100,11 @@ namespace simple_pmc_mover
         }
 
 
-        public void initialPosition(int[] xbotIds)
+        public void initialPosition(int xbot4, int xbot5, int xbot6, int xbot7)
         {
-            double[] start_x_meters = { 0.419,0.419,0.293,0.293 };
-            double[] start_y_meters = { 0.070, 0.248 ,0.070,0.248 };
+            int[] xbotIds = { xbot4, xbot5, xbot6, xbot7 };
+            double[] start_x_meters = { 0.297,0.297,0.423,0.423 };
+            double[] start_y_meters = { 0.248, 0.070 ,0.248,0.070 };
 
             double[] max_speeds = { 0.15,0.15, 0.15,0.15 };
             double[] end_speeds = { 0, 0,0,0 };
--- simple_pmc_mover/simple_pmc_mover/Program.cs
+++ simple_pmc_mover/simple_pmc_mover/Program.cs
@@ -30,7 +30,7 @@ namespace simple_pmc_mover
             Console.WriteLine("Input the ids for the Xbots in the following ");
 
             XBotIDs tempId = _xbotCommand.GetXBotIDS();
-            xbot_ids = new int[tempId.XBotIDsArray.Length];
+            xbot_ids = new int[8];
             for (int i = 0; i < xbot_ids.Length; i++)
             {
                 xbot_ids[i] = Convert.ToInt32(Console.ReadLine());
@@ -41,9 +41,13 @@ namespace simple_pmc_mover
         public void Run()
 
         {
-            int[] xbots1 = { 1, 3 };
-            int[] xbots2 = { 2, 4 };
-            int[] collectedXbots = { 1, 2, 3, 4 };
+            bool tub_been_lifted  = false;
+            bool nest_been_lifted = false;
+            bool moved_together   = false;
+            
+            //int[] collectedXbots = { xbot_ids[4], xbot_ids[5] , xbot_ids[6], xbot_ids[7] };
+            //int[] xbots1 = { xbot_ids[4], xbot_ids[5] };
+            //int[] xbots2 = { xbot_ids[6], xbot_ids[7] };
             do
             {
                 CONNECTIONSTATUS status = connectionHandler.ConnectAndGainMastership();
@@ -88,7 +92,7 @@ namespace simple_pmc_mover
                     Console.WriteLine("4    Move Xbots out of magazine");
                     Console.WriteLine("5    Move tub into magazine");
                     Console.WriteLine("6    Perform Precision test");
-                    Console.WriteLine("7    Perform scissor lift lift");
+                    Console.WriteLine("7    Move tub out from under nest");
                     Console.WriteLine("9    Perform tub lift");
                     Console.WriteLine("ESC  Exit");
 
@@ -98,62 +102,66 @@ namespace simple_pmc_mover
                     switch (keyInfo.KeyChar)
                     {
                         case '0':
-                            selector = 0;
+                            selector = 1;
                             break;
                         case '1':
                             //example 1, run start up routine
-                            status = connectionHandler.ConnectAndGainMastership();
+                            xbotCalibration();
                             break;
                         case '2':
-                            //Move the xbots further apart
-                            movement.initialPosition(collectedXbots);
+                            //initial position for the tub pick up
+                            movement.initialPosition(xbot_ids[4], xbot_ids[5], xbot_ids[6], xbot_ids[7]);
                             break;
                         case '3':
                             //Perform a tub set down
 
-                            movement.MoveRelativeTogether(xbots1, -0.0005, Movement.DIRECTION.Y, 0.0025, 0.005);
-                            movement.MoveRelativeTogether(xbots2, 0.0005, Movement.DIRECTION.Y, 0.0025, 0.005);
+                            movement.MoveRelativeTogether(xbot_ids[4], xbot_ids[6], 0.02, Movement.DIRECTION.Y, 0.0025, 0.005);
+                            movement.MoveRelativeTogether(xbot_ids[5], xbot_ids[7], -0.02, Movement.DIRECTION.Y, 0.0025, 0.005);
                             break;
                         case '4':
                             // Move the tub out
-                            movement.MoveRelativeTogether(collectedXbots, 0.4, Movement.DIRECTION.Y, 0.15, 0.5);
+                            movement.MoveRelativeTogether(xbot_ids[4], xbot_ids[5], 0.52, Movement.DIRECTION.Y, 0.15, 0.5);
+                            movement.MoveRelativeTogether(xbot_ids[6], xbot_ids[7], 0.52, Movement.DIRECTION.Y, 0.15, 0.5);
                             break;
 
                         case '5':
                             // Move the tub in
-                            movement.MoveRelativeTogether(collectedXbots, -0.4, Movement.DIRECTION.Y, 0.15, 0.5);
+                            movement.MoveRelativeTogether(xbot_ids[4], xbot_ids[5], -0.52, Movement.DIRECTION.Y, 0.15, 0.5);
+                            movement.MoveRelativeTogether(xbot_ids[6], xbot_ids[7], -0.52, Movement.DIRECTION.Y, 0.15, 0.5);
                             break;
 
-                        case '6':
-                            //perform the full motion from 1 press
+                        //case '6':
+                        //    //perform the full motion from 1 press
 
-                            // first the initial position
-                            movement.initialPosition(collectedXbots);
-                            //lift the tub
-                            movement.MoveRelativeTogether(xbots1, 0.02, Movement.DIRECTION.Y, 0.0025, 0.005);
-                            movement.MoveRelativeTogether(xbots2, -0.02, Movement.DIRECTION.Y, 0.0025, 0.005);
+                        //    // first the initial position
+                        //    movement.initialPosition(collectedXbots);
+                        //    //lift the tub
+                        //    movement.MoveRelativeTogether(xbots1, 0.02, Movement.DIRECTION.Y, 0.0025, 0.005);
+                        //    movement.MoveRelativeTogether(xbots2, -0.02, Movement.DIRECTION.Y, 0.0025, 0.005);
 
-                            //drive the tub out
-                            movement.MoveRelativeTogether(collectedXbots, 0.6, Movement.DIRECTION.Y, 0.15, 0.5);
-                            //lower the tub
-                            movement.MoveRelativeTogether(xbots1, -0.01, Movement.DIRECTION.Y, 0.0025, 0.005);
-                            movement.MoveRelativeTogether(xbots2, 0.01, Movement.DIRECTION.Y, 0.0025, 0.005);
-                            //lift the tub
-                            movement.MoveRelativeTogether(xbots1, 0.01, Movement.DIRECTION.Y, 0.0025, 0.005);
-                            movement.MoveRelativeTogether(xbots2, -0.01, Movement.DIRECTION.Y, 0.0025, 0.005);
+                        //    //drive the tub out
+                        //    movement.MoveRelativeTogether(collectedXbots, 0.6, Movement.DIRECTION.Y, 0.15, 0.5);
+                        //    //lower the tub
+                        //    movement.MoveRelativeTogether(xbots1, -0.01, Movement.DIRECTION.Y, 0.0025, 0.005);
+                        //    movement.MoveRelativeTogether(xbots2, 0.01, Movement.DIRECTION.Y, 0.0025, 0.005);
+                        //    //lift the tub
+                        //    movement.MoveRelativeTogether(xbots1, 0.01, Movement.DIRECTION.Y, 0.0025, 0.005);
+                        //    movement.MoveRelativeTogether(xbots2, -0.01, Movement.DIRECTION.Y, 0.0025, 0.005);
 
-                            //move it back
-                            movement.MoveRelativeTogether(collectedXbots, -0.6, Movement.DIRECTION.Y, 0.15, 0.5);
+                        //    //move it back
+                        //    movement.MoveRelativeTogether(collectedXbots, -0.6, Movement.DIRECTION.Y, 0.15, 0.5);
 
-                            //set the tub down
-                            movement.MoveRelativeTogether(xbots1, -0.02, Movement.DIRECTION.Y, 0.0025, 0.005);
-                            movement.MoveRelativeTogether(xbots2, 0.02, Movement.DIRECTION.Y, 0.0025, 0.005);
+                        //    //set the tub down
+                        //    movement.MoveRelativeTogether(xbots1, -0.02, Movement.DIRECTION.Y, 0.0025, 0.005);
+                        //    movement.MoveRelativeTogether(xbots2, 0.02, Movement.DIRECTION.Y, 0.0025, 0.005);
 
 
-                            break;
+                        //    break;
 
                         case '7':
-                            scissorTest.execute();
+                            //move tub part ways out from under the nest to lift the tub back up into the mag
+                            movement.MoveRelativeTogether(xbot_ids[4], xbot_ids[5], -0.26, Movement.DIRECTION.Y, 0.15, 0.5);
+                            movement.MoveRelativeTogether(xbot_ids[6], xbot_ids[7], -0.26, Movement.DIRECTION.Y, 0.15, 0.5);
                             break;
                         case '8':
                             for (int i = 0; i < 20; i++)
@@ -164,8 +172,8 @@ namespace simple_pmc_mover
                             break;
                         case '9':
                             // perform a tub lift
-                            movement.MoveRelativeTogether(xbots1, 0.0005, Movement.DIRECTION.Y, 0.0025, 0.005);
-                            movement.MoveRelativeTogether(xbots2, -0.0005, Movement.DIRECTION.Y, 0.0025, 0.005);
+                            movement.MoveRelativeTogether(xbot_ids[4], xbot_ids[6], -0.02, Movement.DIRECTION.Y, 0.0025, 0.005);
+                            movement.MoveRelativeTogether(xbot_ids[5], xbot_ids[7], 0.02, Movement.DIRECTION.Y, 0.0025, 0.005);
 
                             break;
 
@@ -189,7 +197,7 @@ namespace simple_pmc_mover
                     {
                         
                         case '0':
-                            selector = 0;
+                            selector = 1;
                             break;
 
                         case '1':
@@ -220,12 +228,17 @@ namespace simple_pmc_mover
                     Console.WriteLine("1    Reconnect");
                     Console.WriteLine("2    Calibrate XIDs");
                     Console.WriteLine("3    start position for lift");
+                    Console.WriteLine("4    Lift nest");
+                    Console.WriteLine("5    Lower Nest");
+                    Console.WriteLine("6    Move together");
+                    Console.WriteLine("7    Move apart");
+                    Console.WriteLine("9    DEMO");
                     ConsoleKeyInfo keyInfo = Console.ReadKey();
                     switch (keyInfo.KeyChar)
                     {
 
                         case '0':
-                            selector = 0;
+                            selector = 1;
                             break;
 
                         case '1':
@@ -239,40 +252,128 @@ namespace simple_pmc_mover
                             break;
 
                         case '3':
+                            // start position of the gear lifts when performing a denest operation
 
-                            _xbotCommand.LinearMotionSI(0, xbot_ids[0], POSITIONMODE.ABSOLUTE, LINEARPATHTYPE.XTHENY, 0.076, 0.787, 0, 0.005, 0.01);
-                            _xbotCommand.LinearMotionSI(0, xbot_ids[1], POSITIONMODE.ABSOLUTE, LINEARPATHTYPE.XTHENY, 0.076, 0.370, 0, 0.005, 0.01);
+                            _xbotCommand.LinearMotionSI(0, xbot_ids[0], POSITIONMODE.ABSOLUTE, LINEARPATHTYPE.XTHENY, 0.1035, 0.712+0.12, 0, 0.01, 0.01);
+                            _xbotCommand.LinearMotionSI(0, xbot_ids[1], POSITIONMODE.ABSOLUTE, LINEARPATHTYPE.XTHENY, 0.1035, 0.391+0.12, 0, 0.01, 0.01);
 
-                            _xbotCommand.LinearMotionSI(0, xbot_ids[2], POSITIONMODE.ABSOLUTE, LINEARPATHTYPE.XTHENY, 0.588, 0.787- 0.0085,0, 0.005, 0.01);
-                            _xbotCommand.LinearMotionSI(0, xbot_ids[3], POSITIONMODE.ABSOLUTE, LINEARPATHTYPE.XTHENY, 0.588, 0.370-0.0085, 0, 0.005, 0.01);
+                            _xbotCommand.LinearMotionSI(0, xbot_ids[2], POSITIONMODE.ABSOLUTE, LINEARPATHTYPE.XTHENY, 0.6165, 0.720-0.020+0.12,0, 0.01, 0.01);
+                            _xbotCommand.LinearMotionSI(0, xbot_ids[3], POSITIONMODE.ABSOLUTE, LINEARPATHTYPE.XTHENY, 0.6165, 0.399-0.020+0.12, 0, 0.01, 0.01);
 
                             break;
 
                         case '4':
                             //Lift the nest
-                            movement.MoveOpposite(xbot_ids[0], xbot_ids[1], 0.0005, Movement.DIRECTION.Y, 0.001, 0.01);
-                            movement.MoveOpposite(xbot_ids[2], xbot_ids[3], 0.0005, Movement.DIRECTION.Y, 0.001, 0.01);
+                            movement.MoveOpposite(xbot_ids[0], xbot_ids[1], 0.053, Movement.DIRECTION.Y, 0.005, 0.01);
+                            movement.MoveOpposite(xbot_ids[2], xbot_ids[3], 0.053, Movement.DIRECTION.Y, 0.005, 0.01);
 
 
                             break;
                         case '5':
                             //Lower the nest
-                            movement.MoveOpposite(xbot_ids[0], xbot_ids[1], -0.0005, Movement.DIRECTION.Y, 0.001, 0.01);
-                            movement.MoveOpposite(xbot_ids[2], xbot_ids[3], -0.0005, Movement.DIRECTION.Y, 0.001, 0.01);
+                            movement.MoveOpposite(xbot_ids[0], xbot_ids[1], -0.053, Movement.DIRECTION.Y, 0.005, 0.01);
+                            movement.MoveOpposite(xbot_ids[2], xbot_ids[3], -0.053, Movement.DIRECTION.Y, 0.005, 0.01);
                             break;
 
                         case '6':
-                            // move together
-                            movement.MoveOpposite(xbot_ids[2], xbot_ids[0], 0.0005, Movement.DIRECTION.X, 0.001, 0.01);
-                            movement.MoveOpposite(xbot_ids[3], xbot_ids[1], 0.0005, Movement.DIRECTION.X, 0.001, 0.01);
+                            // move together denester together
+                            movement.MoveOpposite(xbot_ids[2], xbot_ids[0], 0.001, Movement.DIRECTION.X, 0.001, 0.01);
+                            movement.MoveOpposite(xbot_ids[3], xbot_ids[1], 0.001, Movement.DIRECTION.X, 0.001, 0.01);
                             break;
 
                         case '7':
-                            // move appart
-                            movement.MoveOpposite(xbot_ids[2], xbot_ids[0], -0.0005, Movement.DIRECTION.X, 0.001, 0.01);
-                            movement.MoveOpposite(xbot_ids[1], xbot_ids[3], -0.0005, Movement.DIRECTION.X, 0.001, 0.01);
+                            // move denester appart
+                            movement.MoveOpposite(xbot_ids[2], xbot_ids[0], -0.001, Movement.DIRECTION.X, 0.001, 0.01);
+                            movement.MoveOpposite(xbot_ids[3], xbot_ids[1], -0.001, Movement.DIRECTION.X, 0.001, 0.01);
                             break;
 
+                        case '9':
+                            WaitUntilTriggerParams time_params = new WaitUntilTriggerParams();
+                            double time = 0;
+
+                            // full Demo
+                            // we assume that we always start after the tubs and gearlifts have been reset using their respective
+                            // Initial position commands
+
+                            // Lift the tub out of nest
+                            time += movement.MoveRelativeTogether(xbot_ids[4], xbot_ids[6], -0.02, Movement.DIRECTION.Y, 0.0025, 0.005);
+                            movement.MoveRelativeTogether(xbot_ids[5], xbot_ids[7], 0.02, Movement.DIRECTION.Y, 0.0025, 0.005);
+
+                            // Move tub to the denester
+                            time += movement.MoveRelativeTogether(xbot_ids[4], xbot_ids[5], 0.52, Movement.DIRECTION.Y, 0.15, 0.5);
+                            movement.MoveRelativeTogether(xbot_ids[6], xbot_ids[7], 0.52, Movement.DIRECTION.Y, 0.15, 0.5);
+
+                            // We now have to wait for all of these motions in order to sync up the gear lifts
+                            time_params.delaySecs = time;
+    
+                            _xbotCommand.WaitUntil(0, xbot_ids[0], TRIGGERSOURCE.TIME_DELAY, time_params);
+                            _xbotCommand.WaitUntil(0, xbot_ids[1], TRIGGERSOURCE.TIME_DELAY, time_params);
+                            _xbotCommand.WaitUntil(0, xbot_ids[2], TRIGGERSOURCE.TIME_DELAY, time_params);
+                            _xbotCommand.WaitUntil(0, xbot_ids[3], TRIGGERSOURCE.TIME_DELAY, time_params);
+
+                            time = 0;
+                            // Lower the hooks
+                            time += movement.MoveOpposite(xbot_ids[0], xbot_ids[1], -0.053, Movement.DIRECTION.Y, 0.005, 0.01);
+                            movement.MoveOpposite(xbot_ids[2], xbot_ids[3], -0.053, Movement.DIRECTION.Y, 0.005, 0.01);
+
+                            // Grasp the nest
+                            time += movement.MoveOpposite(xbot_ids[2], xbot_ids[0], 0.008, Movement.DIRECTION.X, 0.001, 0.01);
+                            movement.MoveOpposite(xbot_ids[3], xbot_ids[1], 0.008, Movement.DIRECTION.X, 0.001, 0.01);
+
+                            // Lift the nest
+                            movement.MoveOpposite(xbot_ids[0], xbot_ids[1], 0.053, Movement.DIRECTION.Y, 0.005, 0.01);
+                            time += movement.MoveOpposite(xbot_ids[2], xbot_ids[3], 0.053, Movement.DIRECTION.Y, 0.005, 0.01);
+
+
+
+
+                            time_params.delaySecs = time;
+
+
+                            // The scissor lifts waits for the gear lift to remove the nest
+                            _xbotCommand.WaitUntil(0, xbot_ids[4], TRIGGERSOURCE.TIME_DELAY, time_params);
+                            _xbotCommand.WaitUntil(0, xbot_ids[5], TRIGGERSOURCE.TIME_DELAY, time_params);
+                            _xbotCommand.WaitUntil(0, xbot_ids[6], TRIGGERSOURCE.TIME_DELAY, time_params);
+                            _xbotCommand.WaitUntil(0, xbot_ids[7], TRIGGERSOURCE.TIME_DELAY, time_params);
+                            time = 0;
+
+
+                            // the gear lift lands
+                            _xbotCommand.LevitationCommand(xbot_ids[0], LEVITATEOPTIONS.LAND);
+                            _xbotCommand.LevitationCommand(xbot_ids[1], LEVITATEOPTIONS.LAND);
+                            _xbotCommand.LevitationCommand(xbot_ids[2], LEVITATEOPTIONS.LAND);
+                            _xbotCommand.LevitationCommand(xbot_ids[3], LEVITATEOPTIONS.LAND);
+
+                            // Scissor Lift lowers
+                            time += movement.MoveRelativeTogether(xbot_ids[4], xbot_ids[6], 0.02, Movement.DIRECTION.Y, 0.0025, 0.005);
+                            movement.MoveRelativeTogether(xbot_ids[5], xbot_ids[7], -0.02, Movement.DIRECTION.Y, 0.0025, 0.005);
+
+                            // Scissor Lift out of range for nest
+                            time += movement.MoveRelativeTogether(xbot_ids[4], xbot_ids[5], -0.26, Movement.DIRECTION.Y, 0.15, 0.5);
+                            movement.MoveRelativeTogether(xbot_ids[6], xbot_ids[7], -0.26, Movement.DIRECTION.Y, 0.15, 0.5);
+
+                            // Scissor lifts the nest
+                            time += movement.MoveRelativeTogether(xbot_ids[4], xbot_ids[6], -0.02, Movement.DIRECTION.Y, 0.0025, 0.005);
+                            movement.MoveRelativeTogether(xbot_ids[5], xbot_ids[7], 0.02, Movement.DIRECTION.Y, 0.0025, 0.005);
+
+
+                            // Scissor lift moves fully back
+                            time += movement.MoveRelativeTogether(xbot_ids[4], xbot_ids[5], -0.26, Movement.DIRECTION.Y, 0.15, 0.5);
+                            movement.MoveRelativeTogether(xbot_ids[6], xbot_ids[7], -0.26, Movement.DIRECTION.Y, 0.15, 0.5);
+
+                            // Scissor lift places the tub
+                            time += movement.MoveRelativeTogether(xbot_ids[4], xbot_ids[6], 0.02, Movement.DIRECTION.Y, 0.0025, 0.005);
+                            movement.MoveRelativeTogether(xbot_ids[5], xbot_ids[7], -0.02, Movement.DIRECTION.Y, 0.0025, 0.005);
+
+                            // Scissor lift moves out under nest
+                            time += movement.MoveRelativeTogether(xbot_ids[4], xbot_ids[5], 0.52, Movement.DIRECTION.Y, 0.15, 0.5);
+                            movement.MoveRelativeTogether(xbot_ids[6], xbot_ids[7], 0.52, Movement.DIRECTION.Y, 0.15, 0.5);
+
+                            // Scissor lift seperates
+                            time += movement.MoveOpposite(xbot_ids[6], xbot_ids[4], -0.06, Movement.DIRECTION.X, 0.01, 0.01);
+                            movement.MoveOpposite(xbot_ids[7], xbot_ids[5], -0.06, Movement.DIRECTION.X, 0.01, 0.01);
+
+                            break;
 
 
 
