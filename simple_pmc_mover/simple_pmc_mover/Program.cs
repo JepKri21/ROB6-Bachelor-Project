53e6d2a1b22de32955f2df01681c1213d9913de6
JepKri21 <93719034+JepKri21@users.noreply.github.com>
2024-05-21 14:12:05 +0200

    update

--- simple_pmc_mover/simple_pmc_mover/Program.cs
+++ simple_pmc_mover/simple_pmc_mover/Program.cs
@@ -37,7 +37,7 @@ namespace simple_pmc_mover
 ____________________________________________________________________| ";
 
         //If ID is always the same use outcommented line
-        public int[] xbot_ids = {5,2,8,3,1,4,6,7};
+        public int[] xbot_ids = {1,5,8,3,4,7,6,2};
         //int[] xbot_ids;
 
        
